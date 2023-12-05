6e71b30bed384a7dcf0d979760149038193bf7b6
EveBaker <evebaker0162@gmail.com>
2023-12-05 11:11:53 -0600

    work in prog

--- script.js
+++ script.js
@@ -9,8 +9,21 @@ $(document).ready(function() {
         success: function(request) {
             $("MyLoader").hide();
             for (const i = 0; i < request.length; i++) {
-                const $html = $()
+                const $html = $(`
+                <div class="carousel-item carousel-item-content ${i === 0 ? 'active' : ''}">
+                <div class="row">
+                    <div class="col-sm-3 text-center">
+                        <img class="rounded-circle" src=${request[i].pic_url} class="d-block w-100" alt="random alt image">
+                    </div>
+                    <div class="col-sm-8 ml-3 d-flex flex-column">
+                        <div>&lt;&lt; ${request[i].text} &gt;&gt;</div>
+                        <div class="font-weight-bold mt-3">${request[i].name}</div>
+                        <div>${request[i].title}</div>
+                    </div>
+                </div>
+            </div>`);
+            $("#quotesInner").append($html);
             }
-        }
+        },
     })
-})
+});
