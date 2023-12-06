9d69700b70e0534437f8ce07814c9922ffa1dc15
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-12-06 20:39:57 +0000

    updating quote carousel

--- script.js
+++ script.js
@@ -2,30 +2,35 @@
 $(document).ready(function() {
     $.ajax({
         url: 'https://smileschool-api.hbtn.info/quotes',
-        type: 'GET',
+        type: 'get',
         beforeSend: function() {
             $("#loaderDiv").show();
         },
         success: function(response) {
             $("#loaderDiv").hide();
-            for (const i = 0; i < response.length; i++) {
+            for (let i = 0; i < response.length; i++) {
                 const $html = $(`
-                <div class="carousel-item carousel-item-content ${i === 0 ? 'active' : ''}">
-                <div class="row">
-                    <div class="col-sm-3 text-center">
-                        <img class="rounded-circle" src=${response[i].pic_url} class="d-block w-100" alt="random alt image">
+                    <div class="carousel-item carousel-item-content ${i === 0 ? 'active' : ''}">
+                        <div class="row">
+                            <div class="col-sm-3 text-center">
+                                <img class="rounded-circle d-block w-100" src="${response[i].pic_url}" alt="Profile picture of ${response[i].name}">
+                            </div>
+                            <div class="col-sm-8 ml-3 d-flex flex-column">
+                                <div>&lt;&lt; ${response[i].text} &gt;&gt;</div>
+                                <div class="font-weight-bold mt-3">${response[i].name}</div>
+                                <div>${response[i].title}</div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-sm-8 ml-3 d-flex flex-column">
-                        <div>&lt;&lt; ${response[i].text} &gt;&gt;</div>
-                        <div class="font-weight-bold mt-3">${response[i].name}</div>
-                        <div>${response[i].title}</div>
-                    </div>
-                </div>
-            </div>`);
-            $("#carousel").append($html);
+                `);
+                $("#carousel-inner").append($html);
             }
         },
-    })
+        error: function(xhr, status, error) {
+            console.error("Error in AJAX request: " + error);
+            $("#loaderDiv").hide();
+        }
+    });
 });
 
 $.ajax({
