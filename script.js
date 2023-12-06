71f7d0fda827462aaa56f6cd46a42e2a61fce8a3
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-12-06 21:13:15 +0000

    still messing with all loaders, will come back to homepage later

--- script.js
+++ script.js
@@ -80,3 +80,52 @@ $.ajax({
         }
     },
 });
+
+$.ajax({
+    url: 'https://smileschool-api.hbtn.info/latest-videos',
+    type: "get",
+    beforeSend: function() {
+      $("#myloader").show(); // Show the loader before making the request
+    },
+    success: function(response) {
+      $("#myloader").hide();
+      for (let i = 0; i < response.length; i++) {
+        let $card_stars = '';
+        let $play_button = '<img src="images/play.png" class="play-btn1" alt="..."   width="64" height="64">';
+        for (let j = 0; j < response[i].star; j++) {
+          $card_stars += '<img src="images/star_on.png" class=" star-size mr-1" alt="..." width="15" height="15">';
+        }
+        for (let j = 0; j < 5 - response[i].star; j++) {
+          $card_stars += '<img src="images/star_off.png" class=" star-size" alt="..." width="15" height="15">';
+        }
+        let $html = $(`
+          <div class="text-center col-12 col-sm-6 col-md-3">
+            <div class="carousel-item active">
+              <img class="w-100" src="${response[i].thumb_url}" alt="">
+              <div class="mx-3">
+                <div class="font-weight-bold text-dark text-left mt-3">
+                  ${response[i].title}
+                </div>
+                <div class="text-secondary text-left mt-3 mb-3">
+                  ${response[i]["sub-title"]}
+                </div>
+                <div class="">
+                  ${$play_button}
+                  <img src="${response[i].author_pic_url}" class="carousel-img rounded-circle mr-3" alt="..."   width="30" height="30">
+                  <div class="pink-text font-weight-bold">${response[i].author}</div>
+                </div>
+                <div class="d-flex justify-content-between mb-5">
+                  <div class="d-flex pt-1">
+                    ${$card_stars}
+                  </div>
+                  <div class="purple-text font-weight-bold">
+                    ${response[i].duration}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`);
+        $("#latestvideos").append($html);
+      }
+    }
+  });
