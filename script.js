c81bfd64a8f00057d5f4206545b145fd74bf72aa
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-12-06 15:10:37 +0000

    still working on loader

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 $(document).ready(function() {
     $.ajax({
         url: 'https://smileschool-api.hbtn.info/quotes',
-        type: 'get',
+        type: 'GET',
         beforeSend: function() {
             $("#loaderDiv").show();
         },
-        success: function(request) {
+        success: function(response) {
             $("#loaderDiv").hide();
-            for (const i = 0; i < request.length; i++) {
+            for (const i = 0; i < response.length; i++) {
                 const $html = $(`
                 <div class="carousel-item carousel-item-content ${i === 0 ? 'active' : ''}">
                 <div class="row">
                     <div class="col-sm-3 text-center">
-                        <img class="rounded-circle" src=${request[i].pic_url} class="d-block w-100" alt="random alt image">
+                        <img class="rounded-circle" src=${response[i].pic_url} class="d-block w-100" alt="random alt image">
                     </div>
                     <div class="col-sm-8 ml-3 d-flex flex-column">
-                        <div>&lt;&lt; ${request[i].text} &gt;&gt;</div>
-                        <div class="font-weight-bold mt-3">${request[i].name}</div>
-                        <div>${request[i].title}</div>
+                        <div>&lt;&lt; ${response[i].text} &gt;&gt;</div>
+                        <div class="font-weight-bold mt-3">${response[i].name}</div>
+                        <div>${response[i].title}</div>
                     </div>
                 </div>
             </div>`);
